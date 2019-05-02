dc47468371d6777ecddb8441db466f777543816b
Kristaps Grinbergs <fassko@gmail.com>
2019-05-02 14:07:57 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 
 import PackageDescription
 
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["QminderAPI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.0.6")
+        .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.1.0")
     ],
     targets: [
         .target(
