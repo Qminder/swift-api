1b901399a65da9faef382581cb4ecb4266a0bfb1
Stanislav Deviatykh <stanislav@qminderapp.com>
2023-03-01 15:01:03 +0200

    feat(api): use tv-pairing-code service

--- Sources/QminderAPIEndpointProtocol.swift
+++ Sources/QminderAPIEndpointProtocol.swift
@@ -10,56 +10,79 @@ import Foundation
 
 /// Qminder API endpoint protocol
 protocol QminderAPIEndpointProtocol {
-  
-  /// Endpoint path
-  var path: String { get }
-  
-  /// Parameters
-  var parameters: [String: Any] { get }
-  
-  /// HTTP methods
-  var method: HTTPMethod { get }
-  
-  /// Is API key needed
-  var apiKeyNeeded: Bool { get }
-  
-  /// Encoding: none or JSON
-  var encoding: ParameterEncoding { get }
+    
+    /// Endpoint path
+    var path: String { get }
+    
+    /// Parameters
+    var parameters: [String: Any] { get }
+    
+    /// HTTP methods
+    var method: HTTPMethod { get }
+    
+    /// Is API key needed
+    var apiKeyNeeded: Bool { get }
+    
+    /// Encoding: none or JSON
+    var encoding: ParameterEncoding { get }
 }
 
 extension QminderAPIEndpointProtocol {
-  
-  /**
-   Create request
-  */
-  internal func request(serverAddress: String, apiKey: String?) throws -> URLRequest {
-    var url = URLComponents(string: "\(serverAddress)\(path)")!
     
-    if encoding == .none {
-      url.queryItems = parameters.map {
-        URLQueryItem(name: $0, value: String(describing: $1))
-      }
+    func requestUnauthorised(serverAddress: String) throws -> URLRequest {
+        var url = URLComponents(string: "\(serverAddress)\(path)")!
+        
+        if encoding == .none {
+            url.queryItems = parameters.map {
+                URLQueryItem(name: $0, value: String(describing: $1))
+            }
+        }
+        
+        var request = URLRequest(url: url.url!)
+        request.httpMethod = method.rawValue
+        
+        if encoding == .json {
+            let jsonData = try? JSONSerialization.data(withJSONObject: parameters, options: [])
+            
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.setValue("application/json", forHTTPHeaderField: "Accept")
+            request.httpBody = jsonData
+        }
+        
+        return request
     }
     
-    var request = URLRequest(url: url.url!)
-    request.httpMethod = method.rawValue
-    
-    if apiKeyNeeded {
-      guard let key = apiKey else {
-        throw QminderError.apiKeyNotSet
-      }
-      
-      request.setValue(key, forHTTPHeaderField: "X-Qminder-REST-API-Key")
+    /**
+     Create request
+     */
+    internal func request(serverAddress: String, apiKey: String?) throws -> URLRequest {
+        var url = URLComponents(string: "\(serverAddress)\(path)")!
+        
+        if encoding == .none {
+            url.queryItems = parameters.map {
+                URLQueryItem(name: $0, value: String(describing: $1))
+            }
+        }
+        
+        var request = URLRequest(url: url.url!)
+        request.httpMethod = method.rawValue
+        
+        if apiKeyNeeded {
+            guard let key = apiKey else {
+                throw QminderError.apiKeyNotSet
+            }
+            
+            request.setValue(key, forHTTPHeaderField: "X-Qminder-REST-API-Key")
+        }
+        
+        if encoding == .json {
+            let jsonData = try? JSONSerialization.data(withJSONObject: parameters, options: [])
+            
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.setValue("application/json", forHTTPHeaderField: "Accept")
+            request.httpBody = jsonData
+        }
+        
+        return request
     }
-    
-    if encoding == .json {
-      let jsonData = try? JSONSerialization.data(withJSONObject: parameters, options: [])
-      
-      request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-      request.setValue("application/json", forHTTPHeaderField: "Accept")
-      request.httpBody = jsonData
-    }
-    
-    return request
-  }
 }
