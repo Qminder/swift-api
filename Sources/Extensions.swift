7c4cabdc6a0b8d863f77621464494ae2bc1a789e
Siim Raud <2ndalpha@gmail.com>
2023-05-29 12:06:24 +0300

    chore: removed curl logging (#331)

--- Sources/Extensions.swift
+++ Sources/Extensions.swift
@@ -1,10 +1,3 @@
-//
-//  Extensions.swift
-//  Alamofire
-//
-//  Created by Kristaps Grinbergs on 26/09/2017.
-//
-
 import Foundation
 
 public extension JSONDecoder {
@@ -89,42 +82,5 @@ public extension Dictionary where Key == String, Value == Any {
     guard let value = optionalValue else { return }
     
     self[key] = value
-  }
-}
-
-extension URLRequest: Loggable {
-  
-  /**
-   Print curl String from request
-  */
-  public func printCurlString() {
-    #if DEBUG
-      log("\(curlString)")
-    #endif
-  }
-  
-  /// curl String
-  public var curlString: String {
-    var result = "curl -k "
-    
-    if let method = httpMethod {
-      result += "-X \(method) \\\n"
-    }
-    
-    if let headers = allHTTPHeaderFields {
-      for (header, value) in headers {
-        result += "-H \"\(header): \(value)\" \\\n"
-      }
-    }
-    
-    if let body = httpBody, !body.isEmpty, let string = String(data: body, encoding: .utf8), !string.isEmpty {
-      result += "-d '\(string)' \\\n"
-    }
-    
-    if let url = url {
-      result += url.absoluteString
-    }
-    
-    return result
   }
 }
