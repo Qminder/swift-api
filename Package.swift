f836fb59f853940fa325180d64a73d68ea8e1e4b
Siim Raud <siim@qminder.com>
2024-12-20 11:28:30 +0200

    No deps

--- Package.swift
+++ Package.swift
@@ -9,13 +9,9 @@ let package = Package(
             name: "QminderAPI",
             targets: ["QminderAPI"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.1.2")
-    ],
     targets: [
         .target(
             name: "QminderAPI",
-            dependencies: ["Starscream"],
             path: "Sources")
     ],
     swiftLanguageVersions: [.v5]
